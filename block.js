813a37011bc9eeabb8b0d4f8f3e575f4c1205dc9
Ankit Varakala <75377379+AnkitVarakala@users.noreply.github.com>
2021-04-01 12:51:40 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,23 @@
+class BLock{
+    constructor(x,y,width,height){
+    var options = {
+        restitution : 0.4,
+        friction : 1.0,
+        //isSatic true
+    }
+    this .body = BOdies.rectangle(x,y,heigth,options);
+    this.width = width;
+    this.height = height;
+    world.add(world, this.body);
+    }
+    display(){
+        var angle = this.body.angle;
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle)
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+}
